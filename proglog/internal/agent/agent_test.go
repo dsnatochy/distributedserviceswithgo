4fc6c225c8ef96e351ccaf3786e5a79836c18232
Dennis Natochy <dnatochy@salesforce.com>
2021-11-05 15:23:10 -0700

    Finished Ch. 7 Server-to-Server Service Discovery

---
+++ proglog/internal/agent/agent_test.go
@@ -0,0 +1,122 @@
+package agent_test
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/travisjeffery/go-dynaport"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	api "github.com/dsnatochy/proglog/api/v1"
+	"github.com/dsnatochy/proglog/internal/agent"
+	"github.com/dsnatochy/proglog/internal/config"
+)
+
+func TestAgent(t *testing.T) {
+	serverTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CertFile:      config.ServerCertFile,
+		KeyFile:       config.ServerKeyFile,
+		CAFile:        config.CAFile,
+		ServerAddress: "127.0.0.1",
+		Server:        true,
+	})
+	require.NoError(t, err)
+
+	peerTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CertFile:      config.RootClientCertFile,
+		KeyFile:       config.RootClientKeyFile,
+		CAFile:        config.CAFile,
+		ServerAddress: "127.0.0.1",
+		Server:        false,
+	})
+	require.NoError(t, err)
+
+	var agents []*agent.Agent
+	for i := 0; i < 3; i++ {
+		ports := dynaport.Get(2)
+		bindAddr := fmt.Sprintf("%s:%d", "127.0.0.1", ports[0])
+		rpcPort := ports[1]
+
+		dataDir, err := ioutil.TempDir("", "agent-test-log")
+		assert.NoError(t, err)
+
+		var startJoinAddrs []string
+		if i != 0 {
+			startJoinAddrs = append(startJoinAddrs, agents[0].Config.BindAddr)
+		}
+
+		agent, err := agent.New(agent.Config{
+			ServerTLSConfig: serverTLSConfig,
+			PeerTLSConfig:   peerTLSConfig,
+			DataDir:         dataDir,
+			BindAddr:        bindAddr,
+			RPCPort:         rpcPort,
+			NodeName:        fmt.Sprintf("%d", i),
+			StartJoinAddrs:  startJoinAddrs,
+			ACLModelFile:    config.ACLModelFile,
+			ACLPolicyFile:   config.ACLPolicyFile,
+		})
+		assert.NoError(t, err)
+
+		agents = append(agents, agent)
+	}
+	defer func() {
+		for _, agent := range agents {
+			err := agent.Shutdown()
+			assert.NoError(t, err)
+			assert.NoError(t, os.RemoveAll(agent.Config.DataDir))
+		}
+	}()
+	time.Sleep(3 * time.Second)
+
+	leaderClient := client(t, agents[0], peerTLSConfig)
+	produceResponse, err := leaderClient.Produce(
+		context.Background(),
+		&api.ProduceRequest{
+			Record: &api.Record{
+				Value: []byte("foo"),
+			},
+		},
+	)
+	assert.NoError(t, err)
+	consumeResponse, err := leaderClient.Consume(
+		context.Background(),
+		&api.ConsumeRequest{
+			Offset: produceResponse.Offset,
+		},
+	)
+	assert.NoError(t, err)
+	assert.Equal(t, consumeResponse.Record.Value, []byte("foo"))
+
+	// wait until replication has finished
+	time.Sleep(3 * time.Second)
+
+	followerClient := client(t, agents[1], peerTLSConfig)
+	consumeResponse, err = followerClient.Consume(
+		context.Background(),
+		&api.ConsumeRequest{
+			Offset: produceResponse.Offset,
+		},
+	)
+	assert.NoError(t, err)
+	assert.Equal(t, consumeResponse.Record.Value, []byte("foo"))
+}
+
+func client(t *testing.T, agent *agent.Agent, tlsConfig *tls.Config) api.LogClient {
+	tlsCreds := credentials.NewTLS(tlsConfig)
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(tlsCreds)}
+	rpcAddr, err := agent.Config.RPCAddr()
+	conn, err := grpc.Dial(fmt.Sprintf("%s", rpcAddr), opts...)
+	assert.NoError(t, err)
+	client := api.NewLogClient(conn)
+	return client
+
+}
